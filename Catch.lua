4221e2a3024197f3f7d35297b8668952623e3625
Bant3r241 <bobby36ko@outlook.com>
2025-08-10 01:41:27 -0400

    Update Catch.lua

--- Catch.lua
+++ Catch.lua
@@ -2,6 +2,13 @@ if game.PlaceId == 75418531859354 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Xlur │ Catch A Brainrot", HidePremium = false, IntroText = "Xlur", SaveConfig = true, ConfigFolder = "XlurConfig"})
 
+local Main = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4299432428",
+	PremiumOnly = false
+})
+
+
 
 
 
